bb2976862e2e8e2ae0378f60e0b81bb7b78623be
Truong Hoang Dung <checkraiser11@gmail.com>
2018-06-01 14:19:04 +0700

    Fix

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,6 +1,26 @@
 require_relative 'spec_helper'
 
 RSpec.describe 'Testing' do 
+  it 'raises error if increases with negative amount' do
+    aggregate_id = Sequent.new_uuid
+    expect {
+      Store.create_counter(aggregate_id)
+    }.to output("CounterCreated\n").to_stdout
+    expect {
+      Store.increase_counter(aggregate_id, -3)
+    }.to raise_error('Amount must be positive')
+  end
+
+  it 'raises error if decreases with negative amount' do
+    aggregate_id = Sequent.new_uuid
+    expect {
+      Store.create_counter(aggregate_id)
+    }.to output("CounterCreated\n").to_stdout
+    expect {
+      Store.decrease_counter(aggregate_id, -3)
+    }.to raise_error('Amount must be positive')
+  end
+
   it 'creates counter' do
     aggregate_id = Store.new_uuid
     expect {
@@ -27,21 +47,5 @@ RSpec.describe 'Testing' do
     }.to output("CounterCreated\nCounterDecreased\n").to_stdout
     
     expect(Store.counter_record.find_by(aggregate_id: aggregate_id).amount).to eql(-3)
-  end
-
-  it 'raises error if increases with negative amount' do
-    aggregate_id = Sequent.new_uuid
-    Store.create_counter(aggregate_id)
-    expect {
-      Store.increase_counter(aggregate_id, -3)
-    }.to raise_error('Amount must be positive')
-  end
-
-  it 'raises error if decreases with negative amount' do
-    aggregate_id = Sequent.new_uuid
-    Store.create_counter(aggregate_id)
-    expect {
-      Store.decrease_counter(aggregate_id, -3)
-    }.to raise_error('Amount must be positive')
   end
 end
